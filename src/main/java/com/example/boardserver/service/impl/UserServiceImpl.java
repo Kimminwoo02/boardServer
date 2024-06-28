33e058ed5de342f828b5b43b511f921649d66056
minutaurus <made_power@naver.com>
2024-06-28 13:51:08 +0900

    Mapper 에 CRUD 추가

--- src/main/java/com/example/boardserver/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/boardserver/service/impl/UserServiceImpl.java
@@ -1,26 +1,51 @@
 package com.example.boardserver.service.impl;
 
 import com.example.boardserver.dto.UserDTO;
+import com.example.boardserver.exception.DuplicateIdException;
+import com.example.boardserver.mapper.UserProfileMapper;
 import com.example.boardserver.service.UserService;
+import com.example.boardserver.util.SHA256Util;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
+import static com.example.boardserver.util.SHA256Util.*;
+
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class UserServiceImpl implements UserService {
+    private UserProfileMapper userProfileMapper;
 
 
     @Override
     public void register(UserDTO userProfile) {
+        boolean duplIdResult = isDuplicated(userProfile.getUserId());
+        if( duplIdResult){
+            throw new DuplicateIdException("Duplicated Id Error");
+        }
+        userProfile.setCreateTime(new Date());
+        userProfile.setPassword(encryptionSHA256(userProfile.getPassword()));
+        int insertCount = userProfileMapper.register(userProfile);
 
+        if (insertCount != 1){
+            log.error( "insertMember ERROR {}", userProfile);
+            throw new RuntimeException( "insertUser ERROR! Please Check the Method \n " + "Params : " + userProfile );
+        }
     }
 
     @Override
     public UserDTO login(String id, String password) {
-        return null;
+        String cryptoPassword = encryptionSHA256(password);
+        return userProfileMapper.findByIdAndPassword(id, cryptoPassword);
+
     }
 
     @Override
     public boolean isDuplicated(String id) {
-        return false;
+        return userProfileMapper.idCheck(id)== 1;
     }
 
     @Override
@@ -30,11 +55,29 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void updatePassword(String id, String beforePassword, String afterPassword) {
+        String cryptoPassword = encryptionSHA256(beforePassword);
+        UserDTO memberInfo = userProfileMapper.findByIdAndPassword(id, cryptoPassword);
 
+
+        if(memberInfo == null){
+            memberInfo.setPassword(encryptionSHA256(afterPassword));
+            int insertCount = userProfileMapper.updatePassword(memberInfo);
+        }else{
+            log.error("update Password ERROR {} !",memberInfo);
+            throw new RuntimeException("Incorrect Password");
+        }
     }
 
     @Override
     public void deleteId(String id, String password) {
+        String cryptoPassword = encryptionSHA256(password);
+        UserDTO memberInfo = userProfileMapper.findByIdAndPassword(id, cryptoPassword);
 
+        if(memberInfo != null){
+            int deleteCount = userProfileMapper.deleteUserProfile(id);
+        } else{
+            log.error( "deleteId ERROR {}!", memberInfo);
+            throw new RuntimeException(" Incorrect Password");
+        }
     }
 }
