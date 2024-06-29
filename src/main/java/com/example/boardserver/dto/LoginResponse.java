43c8af29a961566bbc0f00c2e6d267e2db825bb5
minutaurus <made_power@naver.com>
2024-06-29 19:24:10 +0900

    LoginDTO 추가

---
+++ src/main/java/com/example/boardserver/dto/LoginResponse.java
@@ -0,0 +1,7 @@
+package com.example.boardserver.dto;
+
+public class LoginResponse {
+    public static LoginResponse success(UserDTO userInfo) {
+        return null;
+    }
+}
---
+++ src/main/java/com/example/boardserver/dto/UserLoginRequest.java
@@ -0,0 +1,14 @@
+package com.example.boardserver.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class UserLoginRequest {
+    private String userId;
+    private String password;
+
+}
