7c475f6dbf86bee17123cccfe550cbd0c6bbe035
minutaurus <made_power@naver.com>
2024-06-29 19:24:39 +0900

    REST API 구현

--- src/main/java/com/example/boardserver/controller/UserController.java
+++ src/main/java/com/example/boardserver/controller/UserController.java
@@ -1,10 +1,17 @@
 package com.example.boardserver.controller;
 
+import com.example.boardserver.dto.LoginResponse;
+import com.example.boardserver.dto.UserDTO;
+import com.example.boardserver.dto.UserInfoResponse;
+import com.example.boardserver.dto.UserLoginRequest;
 import com.example.boardserver.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.catalina.manager.util.SessionUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -13,4 +20,88 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+    private static LoginResponse loginResponse;
+
+    @PostMapping("/sign-up")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void signUp(@RequestBody UserDTO userDTO){
+        if(UserDTO.hasNullDataBeforeRegister(userDTO)){
+            throw new RuntimeException("Please check information");
+        }
+        userService.register(userDTO);
+    }
+
+    @PostMapping("/sign-in")
+    public HttpStatus login (@RequestBody UserLoginRequest userLoginRequest, HttpSession session){
+        ResponseEntity<LoginResponse> responseEntity = null;
+        String id = userLoginRequest.getUserId();
+        String password = userLoginRequest.getPassword();
+
+        UserDTO userInfo = userService.login(id,password);
+
+        if (userInfo ==  null){
+            return HttpStatus.NOT_FOUND;
+        }else if (userInfo != null){
+            loginResponse = LoginResponse.success(userInfo);
+            if(userInfo.getStatus() == (UserDTO.Status.ADMIN))
+                SessionUtil.setLoginAdminId(session,id);
+            else
+                SessionUtil.setLoginMemberId(session,id);
+
+            responseEntity = new ResponseEntity<LoginResponse>(loginResponse,HttpStatus.OK);
+        } else{
+            throw new RuntimeException("Login Error! ");
+        }
+
+        return HttpStatus.OK;
+
+    }
+
+    @GetMapping("my-info")
+    public UserInfoResponse memberInfo(HttpSession session){
+        String id = SessionUtil.getLoginMemberId(session);
+        if( id == null) id = SessionUtil.getLoginAdminId(session);
+        UserDTO memberInfo = userService.getUserInfo(id);
+        return new UserInfoResponse(memberInfo);
+    }
+
+    @PutMapping("logout")
+    public void logout( HttpSession session ){
+        SessionUtil.clear(session);
+    }
+
+    @PatchMapping("password")
+    public ResponseEntity<LoginResponse> updateUserPassword(@RequestBody UserUpdatePasswordRequest userUpdatePasswordRequest, HttpSession session) {
+        ResponseEntity<LoginResponse> responseEntity = null;
+        String id = SessionUtil.getLoginMemberId(session);
+        String beforePassword = userUpdatePasswordRequest.getBeforePassword();
+        String afterPassword = userUpdatePasswordRequest.getAfterPassword();
+
+
+        try {
+            userService.updatePassword(id, beforePassword,afterPassword);
+            ResponseEntity.ok(new ResponseEntity<LoginResponse>(loginResponse, HttpStatus.OK));
+        } catch(IllegalArgumentException e){
+            log.error("updatePassword Failed", e);
+            responseEntity = new ResponseEntity<LoginResponse>(HttpStatus.BAD_REQUEST);
+        }
+
+        return responseEntity;
+
+    }
+
+    @DeleteMapping
+    public ResponseEntity<LoginResponse> deleteId(@RequestBody UserDeleteId userDeleteId, HttpSession session){
+        ResponseEntity<LoginResponse> responseEntity = null;
+        String id = SessionUtil.getLoginMemberId(session);
+
+        try{
+            userService.deleteId(id, userDeleteId.getPassword());
+            responseEntity = new ResponseEntity<LoginResponse>(loginResponse,HttpStatus.OK);
+        }catch(RuntimeException e){
+            log.error("deleteId 실패");
+            responseEntity = new ResponseEntity<LoginResponse>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
 }
